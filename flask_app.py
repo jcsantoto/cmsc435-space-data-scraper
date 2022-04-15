f0118afd116f0e66b15ba8e77c4c901b1536b1a7
Johnny Truong <jtruong5@umd.edu>
2022-04-15 17:53:27 -0400

    Updated graph formatting and flask dependencies

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from solar_weather_plotter import SolarWeatherPlotter
+from solar_weather_fetcher import SolarWeatherFetcher
 
 app = Flask(__name__)
 
@@ -9,8 +9,8 @@ def home():
 
 @app.route("/temperature")
 def temperature():
-    temperature = SolarWeatherPlotter._get_solar_wind_data("https://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json", "temperature")
-    time =  SolarWeatherPlotter._get_solar_wind_data("https://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json","time_tag")
+    temperature = SolarWeatherFetcher._get_solar_wind_data("https://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json", "temperature")
+    time =  SolarWeatherFetcher._get_solar_wind_data("https://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json","time_tag")
     return render_template("test.html",data1=temperature, data2=time)
 
 if __name__ == "__main__":
